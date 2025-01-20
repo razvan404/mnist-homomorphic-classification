f4319b549c6f013521bf81e2a12d70182cd945d5
razvan404 <razvanpetec@gmail.com>
2025-01-20 23:27:20 +0200

    Add the CKKS inference pipeline

---
+++ ckks/__init__.py
@@ -0,0 +1,4 @@
+from .ckks_classifier import CkksCompatibleMnistClassifier
+from .utils import create_ckks_context
+
+__all__ = ["CkksCompatibleMnistClassifier", "create_ckks_context"]
---
+++ ckks/tests.py
@@ -0,0 +1,25 @@
+import tqdm
+import torch
+import tenseal as ts
+
+from classifier import MnistClassifier
+from ckks.ckks_classifier import CkksCompatibleMnistClassifier
+from ckks.utils import create_ckks_context, encrypt_data, decrypt_data
+
+
+def test_ckks_classifier(context: ts.Context):
+    classifier = MnistClassifier()
+    classifier.eval()
+    enc_classifier = CkksCompatibleMnistClassifier(classifier, windows_nb=121)
+
+    batch_example = torch.randint(0, 255, (4, 1, 28, 28)) / 255.
+    for input_example in tqdm.tqdm(batch_example):
+        input_enc = encrypt_data(context, input_example)
+        output_enc = enc_classifier(input_enc)
+        output = decrypt_data(context, output_enc)
+        assert output.shape == (10,)
+
+
+if __name__ == "__main__":
+    context = create_ckks_context()
+    test_ckks_classifier(context)
