8f8b0ae0274c854fbccfa23df8dcee7aa2f0447a
razvan404 <razvanpetec@gmail.com>
2025-01-27 02:51:01 +0200

    Make requests async and add a prediction status text

--- demo/demo_client.py
+++ demo/demo_client.py
@@ -1,3 +1,5 @@
+import threading
+
 import requests
 import pygame
 import numpy as np
@@ -24,7 +26,7 @@ class DemoClient:
 
         # Initialize the screen
         self.screen = pygame.display.set_mode(
-            (self.WINDOW_SIZE, self.WINDOW_SIZE + 115)
+            (self.WINDOW_SIZE, self.WINDOW_SIZE + 150)
         )
         pygame.display.set_caption("Draw a Number")
         self.font = pygame.font.Font(None, self.FONT_SIZE)
@@ -46,6 +48,7 @@ class DemoClient:
 
         # Placeholder for predictions
         self.probabilities = [0.0] * 10
+        self.prediction_text = "Prediction: None"
 
         # CKKS Encryptor
         self.encryptor = Encryptor()
@@ -70,7 +73,7 @@ class DemoClient:
             self.screen,
             self.WHITE,
             (0, 0, self.WINDOW_SIZE, self.WINDOW_SIZE),
-            5,  # Border thickness
+            5,
         )
 
     def draw_buttons(self):
@@ -92,25 +95,36 @@ class DemoClient:
         )
         self.screen.blit(reset_text_surface, reset_text_rect)
 
-    def draw_preview(self, mouse_x, mouse_y):
-        grid_x = mouse_x // self.PIXEL_SIZE
-        grid_y = mouse_y // self.PIXEL_SIZE
+    def mouse_in_canvas(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        return 0 < mouse_x < self.WINDOW_SIZE and 0 < mouse_y < self.WINDOW_SIZE
+
+    def iterate_pencil_coords(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        grid_x = (mouse_x - self.PIXEL_SIZE // 2) // self.PIXEL_SIZE
+        grid_y = (mouse_y - self.PIXEL_SIZE // 2) // self.PIXEL_SIZE
+
         for dx in range(2):
             for dy in range(2):
                 if (
                     0 <= grid_x + dx < self.GRID_SIZE
                     and 0 <= grid_y + dy < self.GRID_SIZE
                 ):
-                    pygame.draw.rect(
-                        self.screen,
-                        self.LIGHT_GRAY,
-                        (
-                            (grid_x + dx) * self.PIXEL_SIZE,
-                            (grid_y + dy) * self.PIXEL_SIZE,
-                            self.PIXEL_SIZE,
-                            self.PIXEL_SIZE,
-                        ),
-                    )
+                    yield grid_x + dx, grid_y + dy
+
+    def draw_preview(self):
+        for x, y in self.iterate_pencil_coords():
+            pygame.draw.rect(
+                self.screen,
+                self.LIGHT_GRAY,
+                (
+                    x * self.PIXEL_SIZE,
+                    y * self.PIXEL_SIZE,
+                    self.PIXEL_SIZE,
+                    self.PIXEL_SIZE,
+                ),
+            )
 
     def draw_probabilities(self):
         start_x = 30
@@ -137,17 +151,33 @@ class DemoClient:
             digit_rect = digit_surface.get_rect(center=(start_x + i * spacing, start_y))
             self.screen.blit(digit_surface, digit_rect)
 
+        # Draw prediction text
+        pred_surface = self.font.render(self.prediction_text, True, self.BLACK)
+        pred_rect = pred_surface.get_rect(
+            center=(self.WINDOW_SIZE // 2, self.WINDOW_SIZE + 125)
+        )
+        self.screen.blit(pred_surface, pred_rect)
+
+    def set_prediction_text(self, text: str):
+        self.prediction_text = f"Prediction: {text}"
+
     def process_inference_response(self, response: requests.Response):
         serialized_encrypted_preds = response.json().get("preds", None)
         if serialized_encrypted_preds is None:
             self.probabilities = [0.0] * 10
+            self.set_prediction_text("None")
             return
         encrypted_preds = self.encryptor.deserialize_data(serialized_encrypted_preds)
         preds = self.encryptor.decrypt(encrypted_preds)
 
         # Perform softmax
         e_preds = np.exp(preds - np.max(preds))
-        self.probabilities = e_preds / np.sum(e_preds)
+        self.probabilities = (e_preds / np.sum(e_preds)).tolist()
+
+        # Determine predicted digit and update prediction text
+        predicted_digit = np.argmax(self.probabilities)
+        confidence = self.probabilities[predicted_digit] * 100
+        self.set_prediction_text(f"{predicted_digit} ({confidence:.2f}%)")
 
     def send_inference_request(self):
         try:
@@ -165,22 +195,22 @@ class DemoClient:
                 self.process_inference_response(response)
             else:
                 print(f"Invalid response status: {response.status_code}")
+                self.set_prediction_text("Error")
         except Exception as e:
             print(f"Error: {e}")
+            self.set_prediction_text("Error")
 
     def run(self):
         running = True
         while running:
             self.screen.fill(self.WHITE)
 
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-
             self.draw_grid()
             self.draw_buttons()
             self.draw_probabilities()
 
-            if 0 < mouse_x < self.WINDOW_SIZE and 0 < mouse_y < self.WINDOW_SIZE:
-                self.draw_preview(mouse_x, mouse_y)
+            if self.mouse_in_canvas():
+                self.draw_preview()
 
             pygame.display.flip()
 
@@ -192,26 +222,18 @@ class DemoClient:
                     or event.type == pygame.MOUSEMOTION
                     and pygame.mouse.get_pressed()[0]
                 ):
-                    if mouse_y < self.WINDOW_SIZE:
-                        grid_x = mouse_x // self.PIXEL_SIZE
-                        grid_y = mouse_y // self.PIXEL_SIZE
-                        if (
-                            0 <= grid_x < self.GRID_SIZE
-                            and 0 <= grid_y < self.GRID_SIZE
-                        ):
-                            for dx in range(2):
-                                for dy in range(2):
-                                    if (
-                                        0 <= grid_x + dx < self.GRID_SIZE
-                                        and 0 <= grid_y + dy < self.GRID_SIZE
-                                    ):
-                                        self.drawing[grid_y + dy, grid_x + dx] = 1.0
+                    if self.mouse_in_canvas():
+                        for x, y in self.iterate_pencil_coords():
+                            self.drawing[y, x] = 1.0
+
                     elif event.type == pygame.MOUSEBUTTONDOWN:
                         if self.predict_button_rect.collidepoint(event.pos):
-                            self.send_inference_request()
+                            self.set_prediction_text("Loading...")
+                            threading.Thread(target=self.send_inference_request).start()
                         elif self.reset_button_rect.collidepoint(event.pos):
                             self.drawing.fill(0.0)
                             self.probabilities = [0.0] * 10
+                            self.set_prediction_text("None")
 
         pygame.quit()
 
